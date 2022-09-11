0db5b584206d6b10f282d6192fc98383f47c3264
Mato <tissuepaperarelimited@gmail.com>
2022-09-11 14:33:35 +0800

    Initial Commit

---
+++ Firestore-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Firestore-demo
+//
+//  Created by kittawat phuangsombat on 2022/9/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Firestore-demo/Firestore_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firestore_demoApp.swift
+//  Firestore-demo
+//
+//  Created by kittawat phuangsombat on 2022/9/11.
+//
+
+import SwiftUI
+
+@main
+struct Firestore_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
